326a666ac20a1084fc5ab0c4a9fa0fb87c0ccbd2
A Boopathi <140834773+boopathialagar@users.noreply.github.com>
2026-03-20 20:40:55 +0900

    Time: 0 ms (100.00%) | Memory: 44.1 MB (18.60%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+       if(matrix.length ==0)return false;
+        if(matrix.length==1){
+            for(int i=0;i<matrix[0].length;i++){
+                if(matrix[0][i]==target)return true;
+                
+            }
+            return false;
+        }
+        int position = -1;
+        for(int i=0;i<matrix.length;i++){
+            if( (matrix[i][0]<=target)){
+                position=i;
+            }
+            else{
+                break;
+            }
+        }
+        if(position == -1)return false;
+        for(int i=0;i<matrix[position].length;i++){
+            if(target == matrix[position][i]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
