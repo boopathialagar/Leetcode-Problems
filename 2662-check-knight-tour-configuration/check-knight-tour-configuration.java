0ba61af1f2fcacc083c327d8b2266dbe45b09b45
A Boopathi <140834773+boopathialagar@users.noreply.github.com>
2026-03-31 17:26:49 +0530

    Time: 1 ms (95.18%) | Memory: 46.3 MB (63.55%) - LeetSync

--- 2662-check-knight-tour-configuration/check-knight-tour-configuration.java
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.java
@@ -1,27 +1,30 @@
 class Solution {
     public boolean checkValidGrid(int[][] grid) {
-        if(grid[0][0]!=0)return false;
-        return bfs(grid,0,0,1);
+        if (grid[0][0] != 0)
+            return false;
+        return bfs(grid, 0, 0, 1);
     }
-    int[][] quartinates = {
-        {2,1},
-        {2,-1},
-        {1,2},
-        {1,-2},
-        {-1,2},
-        {-1,-2},
-        {-2,1},
-        {-2,-1}
-    };
-    boolean bfs(int[][] grid,int i,int j,int cell){
 
-        for(int[] q: quartinates){
+    int[][] quartinates = {
+            { 2, 1 },
+            { 2, -1 },
+            { 1, 2 },
+            { 1, -2 },
+            { -1, 2 },
+            { -1, -2 },
+            { -2, 1 },
+            { -2, -1 }
+    };
+
+    boolean bfs(int[][] grid, int i, int j, int cell) {
+
+        for (int[] q : quartinates) {
             int x = q[0];
             int y = q[1];
-            int dx = i+x;
-            int dy = j+y;
-            if((dx>=0 && dy>=0 && dx<grid.length && dy<grid.length) && grid[dx][dy] == cell){
-                return bfs(grid,dx,dy,cell+1);
+            int dx = i + x;
+            int dy = j + y;
+            if ((dx >= 0 && dy >= 0 && dx < grid.length && dy < grid.length) && grid[dx][dy] == cell) {
+                return bfs(grid, dx, dy, cell + 1);
             }
         }
         return cell == grid.length * grid.length;
