888c1ae0043e8ad4e18962a072476c9608a9022e
A Boopathi <140834773+boopathialagar@users.noreply.github.com>
2026-04-14 10:14:46 +0530

    Time: 30 ms (71.94%) | Memory: 146.4 MB (73.71%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,20 +1,14 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
-        int[] result = new int[n - k + 1];
+        int[] result = new int[n-k+1];
         int index = 0;
-        Deque<Integer> dq = new ArrayDeque<>();
-        for(int i = 0; i < n; i++) {
-            if(!dq.isEmpty() && dq.peekFirst() == i - k) {
-                dq.pollFirst();
-            }
-            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
-                dq.pollLast();
-            }
-            dq.offerLast(i);
-            if(i >= k - 1) {
-                result[index++] = nums[dq.peekFirst()];
-            }
+        Deque<Integer> q = new ArrayDeque<>();
+        for(int i=0;i<n;i++){
+            if(!q.isEmpty() &&  i-k == q.peekFirst()) q.pollFirst();
+            while(!q.isEmpty() && nums[i]>nums[q.peekLast()]) q.pollLast();
+            q.offerLast(i);
+            if(i>=k-1) result[index++] = nums[q.peekFirst()];
         }
         return result;
     }
