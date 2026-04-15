65aa3c91f55cae78cd520f276ca0a875abe79e49
A Boopathi <140834773+boopathialagar@users.noreply.github.com>
2026-04-15 13:07:37 +0530

    Time: 1 ms (99.83%) | Memory: 46.9 MB (34.17%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,45 +10,37 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0 || lists == null) return null;
-        return helper(lists, 0, lists.length - 1);
+        if(lists.length==0)return null;
+        return helper(lists,0,lists.length-1);
     }
-
-    public ListNode helper(ListNode[] lists, int start, int end) {
-        if (start == end) {
-            return lists[start];
+    public ListNode helper(ListNode[] lists,int st,int end){
+        if(st == end){
+            return lists[st];
         }
-        if (start + 1 == end) {
-            return merge2List(lists[start], lists[end]);
+        else if(st == end-1){
+           return merge(lists[st],lists[end]);
         }
-        int mid = (start + end) / 2;
-        ListNode left = helper(lists, start,mid);
-        ListNode right = helper(lists, mid + 1, end);
-        return merge2List(left, right);
+        int mid = (st+end) /2;
+        ListNode left =helper(lists,st,mid);
+        ListNode right = helper(lists,mid+1,end);
+        return merge(left,right);
     }
-
-    public ListNode merge2List(ListNode l1, ListNode l2) {
+    public ListNode merge(ListNode l1, ListNode l2){
         ListNode dummy = new ListNode(0);
         ListNode temp = dummy;
-        while (l1 != null && l2 != null) {
-            if (l1.val >= l2.val) {
-                temp.next = l2;
-                l2 = l2.next;
-                 temp = temp.next;
-            }
-            else{
+        while(l1!= null && l2 != null){
+            if(l1.val <l2.val){
                 temp.next = l1;
                 l1 = l1.next;
-                 temp = temp.next;
+                temp = temp.next;
             }
-           
+            else{
+                temp.next = l2;
+                l2 = l2.next;
+                temp = temp.next;
+            }
         }
-        if (l1 != null) {
-            temp.next = l1;
-        }
-        if (l2 != null) {
-            temp.next = l2;
-        }
+        temp.next = (l1 == null)?l2:l1;
         return dummy.next;
     }
 }
