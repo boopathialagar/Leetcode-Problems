31d65bfc385ab50a187da6c73084142231bf0ce4
A Boopathi <140834773+boopathialagar@users.noreply.github.com>
2026-04-16 13:00:47 +0530

    Time: 15 ms (13.94%) | Memory: 46.9 MB (12.08%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -1,29 +1,29 @@
 class Solution {
     public int calculate(String s) {
         int result = 0;
-        int sign = 1;
+        int sign =1;
         Stack<Integer> st = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
+        for(int i =0;i<s.length();i++){
             char c = s.charAt(i);
-            if (Character.isDigit(c)) {
-                int num = 0;
-                while (i < s.length() && Character.isDigit(s.charAt(i))) {
-                    num = num * 10 + (s.charAt(i) - '0');
+            if(Character.isDigit(c)){
+                int num =0;
+                while(i<s.length() && Character.isDigit(s.charAt(i))){
+                    num = num*10 +(s.charAt(i)-'0');
                     i++;
                 }
-                result += sign * num;
                 i--;
-            } else if (c == '-') {
-                sign = -1;
-            } else if (c == '+') {
-                sign = 1;
-            } else if (c == '(') {
+                result = result+(sign*num);
+            }
+            else if(c == '+') sign = 1;
+            else if(c=='-') sign = -1;
+            else if(c == '('){
                 st.push(result);
                 st.push(sign);
-                result = 0;
+                result= 0;
                 sign = 1;
-            } else if (c == ')') {
-                result = result * st.pop();
+            }
+            else if(c == ')'){
+                result = result * st.pop(); 
                 result = result + st.pop();
             }
         }
