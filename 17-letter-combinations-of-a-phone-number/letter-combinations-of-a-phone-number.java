4b7661123477ab9558cee85b1a67f2bc67b33e80
A Boopathi <140834773+boopathialagar@users.noreply.github.com>
2026-04-28 10:37:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (41.76%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,23 +1,24 @@
 class Solution {
     List<String> result = new ArrayList<>();
-    String[] phone = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    
-    public List<String> letterCombinations(String digits) {  
-        combination(0,new StringBuilder(),digits);
-        return result;
+    String[] phone ={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+    public List<String> letterCombinations(String digits) {
+     combination(0,digits,new StringBuilder());
+     return result;   
     }
 
-    public void combination(int index,StringBuilder cur,String digits){
-        if(cur.length() == digits.length()){
+    public void combination(int index,String digits,StringBuilder cur){
+        if(cur.length()==digits.length()){
             result.add(cur.toString());
             return;
         }
         String letters = phone[digits.charAt(index)-'0'];
-
         for(char c:letters.toCharArray()){
             cur.append(c);
-            combination(index+1,cur,digits);
+            combination(index+1,digits,cur);
             cur.deleteCharAt(cur.length()-1);
         }
+
+
     }
 }
