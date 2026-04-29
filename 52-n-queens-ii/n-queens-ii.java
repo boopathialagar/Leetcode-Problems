1e4508791f806c7421dc25a2a8a34b12d1136af9
A Boopathi <140834773+boopathialagar@users.noreply.github.com>
2026-04-29 09:43:12 +0530

    Time: 4 ms (16.26%) | Memory: 42.4 MB (20.32%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    int count=0;
+    public int totalNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+        solve(board,0,result);
+        return count;
+    }
+
+    public void solve(char[][] board,int row,List<List<String>> result){
+        
+        if(row == board.length){
+            List<String> list = new ArrayList<>();
+            for(int i=0;i<board.length;i++){
+                list.add(new String(board[i]));
+            }
+            count++;
+            result.add(list);
+        }
+        for(int col=0;col<board.length;col++){
+            if(check(board,row,col)){
+                board[row][col] = 'Q';
+                solve(board,row+1,result);
+                board[row][col]='.';
+            }
+        }
+    }
+    public boolean check(char[][] board,int row,int col){
+        for(int i=0;i<row;i++){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+            for(int j=0;j<board.length;j++){
+                if(board[i][j]=='Q'){
+                    if(Math.abs(i-row)==Math.abs(j-col)){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
