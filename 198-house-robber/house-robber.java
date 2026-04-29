5fffda78d015da9a2d05dedfa444e9383995f77e
A Boopathi <140834773+boopathialagar@users.noreply.github.com>
2026-04-29 10:11:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (74.64%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,40 +1,17 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        for(int i =0;i<n;i++){
-            if(i==0){
-                dp[i] = nums[i];
+        int[] profit = new int[n+1];
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                profit[0] = nums[0];
+            } else if (i == 1) {
+                profit[1] = Math.max(nums[1], nums[0]);
+            } else {
+                profit[i] = Math.max(nums[i] + profit[i - 2], profit[i - 1]);
             }
-            else if(i==1){
-                dp[i] = Math.max(nums[i],nums[i-1]);
-            }
-            else{
-                dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
-            }
+
         }
-        return dp[n-1];
+        return profit[n - 1];
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
