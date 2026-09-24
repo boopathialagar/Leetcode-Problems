331131fc03ce59dea28fafe78646b01f0cbc69ce
A Boopathi <140834773+boopathialagar@users.noreply.github.com>
2026-09-24 17:51:11 +0900

    Time: 0 ms (100.00%) | Memory: 19.4 MB (11.59%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,6 +1,4 @@
 class Solution:
-
-
     def rev(self,x):
         res = 0
         while x != 0:
@@ -12,13 +10,13 @@ class Solution:
     def reverse(self, x: int) -> int:
             if x >0:
                 res = self.rev(x)
-                if res < 2147483647:
+                if res < 2**31-1:
                     return res
                 return 0
             else:
                 x = x * (-1)
                 res = -1 * self.rev(x) 
-                if res > -2147483648:
+                if res > -2**31:
                     return res
                 else:
                     return 0 
